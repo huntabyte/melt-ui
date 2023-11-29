6fd9bcfadfc3b64aea94b25f1c58c42d655c2a5c
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-29 13:28:05 -0500

    add notice

--- src/lib/internal/helpers/is.ts
+++ src/lib/internal/helpers/is.ts
@@ -65,3 +65,82 @@ export function isNumberString(value: string) {
 export function isObject(value: unknown): value is Record<string, unknown> {
 	return value !== null && typeof value === 'object';
 }
+
+function testUserAgent(re: RegExp) {
+	if (typeof window === 'undefined' || window.navigator == null) return false;
+	return (
+		window.navigator['userAgentData']?.brands.some((brand: { brand: string; version: string }) =>
+			re.test(brand.brand)
+		) || re.test(window.navigator.userAgent)
+	);
+}
+
+function testPlatform(re: RegExp) {
+	if (typeof window === 'undefined' || window.navigator == null) return false;
+	return re.test(window.navigator['userAgentData']?.platform || window.navigator.platform);
+}
+
+export function isMac() {
+	return testPlatform(/^Mac/i);
+}
+
+export function isPhone() {
+	return testPlatform(/^iPhone/i);
+}
+
+export function isIPad() {
+	// iPadOS 13 lies about its user agent & says it's a Mac so we check for touch support too
+	return testPlatform(/^iPad/i) || (isMac() && navigator.maxTouchPoints > 1);
+}
+
+export function isIOS() {
+	return isPhone() || isIPad();
+}
+
+export function isAppleDevice() {
+	return isMac() || isIOS();
+}
+
+export function isChrome() {
+	return testUserAgent(/Chrome/i);
+}
+
+export function isWebKit() {
+	return testUserAgent(/AppleWebKit/i) && !isChrome();
+}
+
+export function isAndroid() {
+	return testUserAgent(/Android/i);
+}
+
+export function isFirefox() {
+	return testUserAgent(/Firefox/i);
+}
+
+// Original licensing for the following method can be found in the
+// NOTICE file in the root directory of this source tree.
+// See https://github.com/facebook/react/blob/3c713d513195a53788b3f8bb4b70279d68b15bcc/packages/react-interactions/events/src/dom/shared/index.js#L74-L87
+
+// Keyboards, Assistive Technologies, and element.click() all produce a "virtual"
+// click event. This is a method of inferring such clicks. Every browser except
+// IE 11 only sets a zero value of "detail" for click events that are "virtual".
+// However, IE 11 uses a zero value for all click events. For IE 11 we rely on
+// the quirk that it produces click events that are of type PointerEvent, and
+// where only the "virtual" click lacks a pointerType field.
+
+export function isVirtualClick(event: MouseEvent | PointerEvent): boolean {
+	// JAWS/NVDA with Firefox.
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	if ((event as any).mozInputSource === 0 && event.isTrusted) {
+		return true;
+	}
+
+	// Android TalkBack's detail value varies depending on the event listener providing the event so we have specific logic here instead
+	// If pointerType is defined, event is from a click listener. For events from mousedown listener, detail === 0 is a sufficient check
+	// to detect TalkBack virtual clicks.
+	if (isAndroid() && (event as PointerEvent).pointerType) {
+		return event.type === 'click' && event.buttons === 1;
+	}
+
+	return event.detail === 0 && !(event as PointerEvent).pointerType;
+}
